d8c81cfc5c63039d8d61fb9c7e57c9f356b434ee
liuyun <yunliuly@cn.ibm.com>
2022-07-13 15:46:46 +0800

    测试通用vue.config.js

--- config/create-vue-config.js
+++ config/create-vue-config.js
@@ -1,16 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const _ = require('lodash')
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require('path')
+// // eslint-disable-next-line @typescript-eslint/no-var-requires
+// const path = require('path')
 
-function createVueConfig (env, customConfig = {}) {
+function createVueConfig (customConfig = {}) {
   console.log('this is create Vue Config')
   return _.merge({
     lintOnSave: true,
     productionSourceMap: false,
-    pages: {
-      index: env.VUE_APP_ENTRY_PAGE_FILE || 'src/main.js'
-    },
     configureWebpack: {
     },
     devServer: {
