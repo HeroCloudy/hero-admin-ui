f059ae61e6e6d607e19dafc6d1b8b79618f855a0
liuyun <yunliuly@cn.ibm.com>
2022-07-14 22:15:48 +0800

    test

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,10 @@
 import { App } from 'vue'
+import { Store } from 'vuex'
 // import { Schema, PropItem, PropItemTypes } from '../libs/components/types'
 
 declare const _default: {
-  install(app: App): void
+  install(app: App): void,
+  registerDefaultModules(store: Store<any>): void
 }
 
 export * from '../libs/components/types'
